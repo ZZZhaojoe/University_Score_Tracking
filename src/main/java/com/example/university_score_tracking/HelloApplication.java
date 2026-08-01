7a8ea68cab5df982cac8de8b2ee8c0f585bac4aa
ZZZhaojoe <tingruizhao625@gmail.com>
2026-08-01 16:35:45 -0600

    10th

--- src/main/java/com/example/university_score_tracking/HelloApplication.java
+++ src/main/java/com/example/university_score_tracking/HelloApplication.java
@@ -11,15 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load(), 900, 900);
-        scene.getStylesheets().add(HelloApplication.class.getResource("style.css").toExternalForm());
-
-
+        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
         stage.setTitle("University GPA & Degree Planner");
         stage.setScene(scene);
-        stage.setMinWidth(800);
-        stage.setMinHeight(750);
         stage.show();
     }
 }
